55c92256e8bb76c6fd608b5ffc339332d411fc32
Manuel Quiroga <quirogamanuel123@gmail.com>
2023-07-07 19:17:39 -0300

    using hooks

---
+++ src/SortingVisualizer/SortingAlgorithms.js
@@ -0,0 +1,17 @@
+const mergeSort= (array) =>{
+
+}
+
+const quickSort = (array) => {
+
+}
+
+const heapSort = (array) => {
+
+}
+
+const bubbleSort = (array) =>{
+
+}
+
+module.exports = {mergeSort, quickSort, heapSort, bubbleSort}
