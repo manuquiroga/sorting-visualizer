84e5070283d7fd98e864c239e20bbb2542d1cad2
Manuel Quiroga <quirogamanuel123@gmail.com>
2023-07-07 20:02:46 -0300

    merge sort animation

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,6 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import './SortingVisualizer.css';
-import {mergeSort, quickSort, heapSort, bubbleSort} from './SortingAlgorithms.js';
+import {mergeSort} from '../SortingAlgorithms/mergeSort';
+
+const ANIMATION_SPEED_MS = 3;
+
+const NUMBER_OF_ARRAY_BARS = 750;
+
+const PRIMARY_COLOR = '#61ddffa4';
+
+const SECONDARY_COLOR = '#ff6b6b';
 
 const SortingVisualizer = () => {
     const [array, setArray] = useState([]);
@@ -12,10 +20,35 @@ const SortingVisualizer = () => {
     const resetArray = () => {
         const newArray = [];    
         for(let i = 0; i < 80; i++){
-            newArray.push(randomIntFromInterval(5, 750));
+            newArray.push(randomIntFromInterval(5, NUMBER_OF_ARRAY_BARS));
         }
         setArray(newArray);
     }
+
+    const visualizeMergeSort = () => {
+        const animations = mergeSort(array);
+        for (let i = 0; i < animations.length; i++) {
+          const arrayBars = document.getElementsByClassName('bar');
+
+          const isColorChange = i % 3 !== 2;
+          if (isColorChange) {
+            const [barOneIdx, barTwoIdx] = animations[i];
+            const barOneStyle = arrayBars[barOneIdx].style;
+            const barTwoStyle = arrayBars[barTwoIdx].style;
+            const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+            setTimeout(() => {
+              barOneStyle.backgroundColor = color;
+              barTwoStyle.backgroundColor = color;
+            }, i * ANIMATION_SPEED_MS);
+          } else {
+            setTimeout(() => {
+              const [barOneIdx, newHeight] = animations[i];
+              const barOneStyle = arrayBars[barOneIdx].style;
+              barOneStyle.height = `${newHeight}px`;
+            }, i * ANIMATION_SPEED_MS);
+          }
+        }
+      }
 
     return(
         <div>
@@ -24,10 +57,10 @@ const SortingVisualizer = () => {
                     <button onClick={() => resetArray()}>New Array</button>
                 </div>
                 <div className="sorting-buttons">
-                    <button onClick={() => mergeSort()}>Merge Sort</button>
-                    <button onClick={() => quickSort()}>Quick Sort</button>
-                    <button onClick={() => heapSort()}>Heap Sort</button>
-                    <button onClick={() => bubbleSort()}>Bubble Sort</button>
+                    <button onClick={visualizeMergeSort}>Merge Sort</button> 
+                    <button >Quick Sort</button>
+                    <button >Heap Sort</button>
+                    <button >Bubble Sort</button>
                 </div>
             </nav>
             <div className='box'>
