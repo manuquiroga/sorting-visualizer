9c051dde584513e8e4c599e61ec0da5f21b4d7f4
Manuel Quiroga <quirogamanuel123@gmail.com>
2023-07-07 15:55:00 -0300

    react-app, random int array, and buttons

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import './SortingVisualizer.css';
+
+export class SortingVisualizer extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            array: [],
+        };
+    }
+
+    mergeSort(){
+
+    }
+
+    quickSort(){
+
+    }
+
+    heapSort(){
+
+    }
+
+    bubbleSort(){
+
+    }
+
+    componentDidMount(){
+        this.resetArray();
+    }
+
+    resetArray(){
+        const array = [];    
+        for(let i = 0; i < 100; i++){
+            array.push(randomIntFromInterval(5, 900));
+        }
+        this.setState({array});
+    }
+
+    render(){
+        const {array} = this.state;
+
+        return (
+            <div className='container'>
+                {array.map((value, key) => (
+                    <div className="bar" key={key} style={{height: `${value}px`}}></div>
+                ))}
+                <button onClick={() => this.resetArray()}>New Array</button>
+                <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                <button onClick={() => this.quickSort()}>Quick Sort</button>
+                <button onClick={() => this.heapSort()}>Heap Sort</button>
+                <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+            </div>
+        );
+    }
+}
+
+function randomIntFromInterval(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export default SortingVisualizer;
+
