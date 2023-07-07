12b4661328278ef422843889dd1311286c30876a
Manuel Quiroga <quirogamanuel123@gmail.com>
2023-07-07 16:02:00 -0300

    temp css

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -32,7 +32,7 @@ export class SortingVisualizer extends React.Component {
     resetArray(){
         const array = [];    
         for(let i = 0; i < 100; i++){
-            array.push(randomIntFromInterval(5, 900));
+            array.push(randomIntFromInterval(5, 800));
         }
         this.setState({array});
     }
@@ -41,15 +41,21 @@ export class SortingVisualizer extends React.Component {
         const {array} = this.state;
 
         return (
-            <div className='container'>
-                {array.map((value, key) => (
-                    <div className="bar" key={key} style={{height: `${value}px`}}></div>
-                ))}
-                <button onClick={() => this.resetArray()}>New Array</button>
-                <button onClick={() => this.mergeSort()}>Merge Sort</button>
-                <button onClick={() => this.quickSort()}>Quick Sort</button>
-                <button onClick={() => this.heapSort()}>Heap Sort</button>
-                <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+            <div>
+                <nav>
+                    <button onClick={() => this.resetArray()}>New Array</button>
+                    <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                    <button onClick={() => this.quickSort()}>Quick Sort</button>
+                    <button onClick={() => this.heapSort()}>Heap Sort</button>
+                    <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+                </nav>
+            
+                <div className='container'>
+                    {array.map((value, key) => (
+                        <div className="bar" key={key} style={{height: `${value}px`}}></div>
+                    ))}
+                    
+                </div>
             </div>
         );
     }
